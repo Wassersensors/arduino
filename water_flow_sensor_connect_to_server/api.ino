3dbfc16cf08c75e88a29770dd5def0e671fa4065
Joel <joel.wasserman8@gmail.com>
2022-10-30 12:05:26 -0700

    updated code to only use a single https connection, not a new one each request

--- water_flow_sensor_connect_to_server/api.ino
+++ water_flow_sensor_connect_to_server/api.ino
@@ -2,32 +2,37 @@
 
 // Path should start with a slash, i.e. /hello/world
 void makeGetRequest(String path) {
-  if (client.connect(SERVER_ADDRESS, PORT)) {
-    client.println("GET " + path + " HTTP/1.1");
-    client.println("Host: " + String(SERVER_ADDRESS));
-    client.println("Connection: close");
-    client.println(); // end HTTP request header
-  } else {
-    Serial.println("connection to pete failed");
+  if (!client.connected()) {
+    Serial.println("client is not connected, connecting again");
+    makeConnection();
   }
+  client.println("GET " + path + " HTTP/1.1");
+  client.println("Host: " + String(SERVER_ADDRESS));
+  client.println(); // end HTTP request header
 }
 
 // Path should start with a slash, i.e. /hello/world
 // Body should be stringified json.
 void makePostRequest(String path, String body) {
-  if (client.connect(SERVER_ADDRESS, PORT)) {
-    // send HTTP request header
-    Serial.println("making post request: " + String(SERVER_ADDRESS));
-    client.println("POST " + path + " HTTP/1.1");
-    client.println("Host: " + String(SERVER_ADDRESS));
-    client.println("Content-Type: application/json");
-    client.println("Accept: */*");
-    client.println("Cache-Control: no-cache");
-    client.println("Content-Length: " + String(body.length()));
-    client.println("Connection: close");
-    client.println(); // end HTTP request header
-    client.println(body);
+  if (!client.connected()) {
+    Serial.println("client is not connected, connecting again");
+    makeConnection();
+  }
+  // send HTTP request header
+  client.println("POST " + path + " HTTP/1.1");
+  client.println("Host: " + String(SERVER_ADDRESS));
+  client.println("Content-Type: application/json");
+  client.println("Accept: */*");
+  client.println("Cache-Control: no-cache");
+  client.println("Content-Length: " + String(body.length()));
+  client.println(); // end HTTP request header
+  client.println(body);
+}
+
+void makeConnection() {
+  if (client.connectSSL(SERVER_ADDRESS, PORT)) {
+    Serial.println("connected to pete");
   } else {
     Serial.println("connection to pete failed");
-  } 
+  }
 }
